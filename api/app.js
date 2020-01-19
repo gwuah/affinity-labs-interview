62b27c533857bd2ce2c4ff057d0885c4abff057d
gwuah <griffithawuah15@gmail.com>
2020-01-19 23:34:03 +0000

    [add] added more code

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const fileUpload = require("express-fileupload");
 const express = require("express");
 const cors = require("cors");
+const utils = require("./utils");
 require("dotenv").config();
 
 const app = express();
@@ -10,7 +11,15 @@ app.use(fileUpload());
 
 app.post("/upload", (req, res) => {
   const path = `${__dirname}/${req.files.csv.name}`;
-  req.files.csv.mv(path);
+  req.files.csv.mv(path, err => {
+    if (!err) {
+      utils.billCreator(path, results => {
+        res.json({ results }).status(200);
+      });
+    } else {
+      res.json({ type: "Failed To Save" }).status(500);
+    }
+  });
 });
 
 app.get("/", (req, res) => {
---
+++ api/utils.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const readline = require("readline");
+
+let readFile = file => {
+  return new Promise((resolve, reject) => {
+    let lines = [];
+    let readStream = fs.createReadStream(file);
+    let reader = readline.createInterface({ input: readStream });
+
+    reader.on("line", line => {
+      if (line.trim()) lines.push(line);
+    });
+
+    reader.on("close", () => {
+      resolve(lines);
+    });
+  });
+};
+
+function calculateHours(start, end) {
+  const [startHrs, startMins] = start.split(":");
+  const [endHrs, endMins] = end.split(":");
+
+  const hrs = parseFloat(endHrs) - parseFloat(startHrs);
+  const mins = parseFloat(endMins) - parseFloat(startMins);
+
+  return hrs + mins / 60;
+}
+
+function rowParser(row) {
+  const [id, rate, company, date, start, end] = row.split(",");
+  const hours = calculateHours(start.trim(), end.trim());
+  const parsedRate = parseFloat(rate.trim());
+  return {
+    id: id.trim(),
+    company: company.trim().toLowerCase(),
+    rate: parsedRate,
+    hours,
+    cost: hours * rate,
+    date: date.trim(),
+    start: start.trim(),
+    end: end.trim()
+  };
+}
+
+function billCreator(path, cb) {
+  readFile(path).then(data => {
+    data.shift(); // remove the first row
+    const parsedData = data.map(rowParser);
+    const database = {};
+    for (log of parsedData) {
+      const company = log.company;
+      if (!database[company]) {
+        database[company] = [log];
+      } else {
+        database[company].push(log);
+      }
+    }
+
+    const results = Object.keys(database).map(company => {
+      let db = { [company]: [] };
+      for (const log of database[company]) {
+        const employeeRecordIndex = db[company].findIndex(
+          employee => employee.id === log.id
+        );
+        if (employeeRecordIndex > -1) {
+          const record = db[company][employeeRecordIndex];
+          const newHours = log.hours + record.hours;
+          db[company][employeeRecordIndex] = {
+            ...record,
+            hours: newHours,
+            cost: record.cost + log.cost
+          };
+        } else {
+          db[company].push({
+            id: log.id,
+            hours: log.hours,
+            rate: log.rate,
+            cost: log.cost
+          });
+        }
+      }
+      return db;
+    });
+
+    console.log(JSON.stringify(results, null, 2));
+
+    cb(results);
+  });
+}
+
+module.exports = {
+  billCreator
+};
